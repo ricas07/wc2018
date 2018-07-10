d59ab122b21535404ff6e43aa7a15340d3563c06
Ricardo Perez <rperez@overstock.com>
2018-07-10 14:43:08 -0600

    formatted picks

--- src/picks/Picks.js
+++ src/picks/Picks.js
@@ -5,15 +5,21 @@ const Picks = () => {
     return (
         <div>
             {Object.keys(brackets).map(person => (
-            <div>
-                <h3>{`${person}'s Picks`}</h3>
-                {Object.keys(brackets[person]).map(rounds =>
-                    <div className="rounds">{rounds}{Object.keys(brackets[person][rounds]).map(games =>
-                        <div className="games">{games}
-                        <div><strong>{brackets[person][rounds][games]}</strong></div>
-                    </div>)}</div>)}
-            </div>)
-        )}
+                <div>
+                    <h3>{`${person}'s Picks`}</h3>
+                    {Object.keys(brackets[person]).map(rounds =>
+                        <div className="rounds">
+                            {rounds}
+                            {Object.keys(brackets[person][rounds]).map(games =>
+                                <div className="games">
+                                {games}
+                                <div>
+                                    <strong>{brackets[person][rounds][games]}</strong>
+                                </div>
+                            </div>)}
+                    </div>)}
+                </div>)
+            )}
         </div>
     );
 };
